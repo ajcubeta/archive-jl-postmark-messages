f871793fe03a1030befe9a5812ee973cf675ef32
Andre Joseph <ajcubeta@gmail.com>
2017-09-29 17:07:06 +0800

    Fix path on outbound webhook messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  match '/outbound_webhooks/delivery', to: 'outbound_webhooks#delivery_outbound_messages', via: [:get]
-  match '/outbound_webhooks/bounce',   to: 'outbound_webhooks#bounce_outbound_messages',   via: [:get]
-  match '/outbound_webhooks/opens',    to: 'outbound_webhooks#open_outbound_messages',     via: [:get]
+  match '/webhook/delivery', to: 'outbound_webhooks#delivery_outbound_messages', via: [:get]
+  match '/webhook/bounce',   to: 'outbound_webhooks#bounce_outbound_messages',   via: [:get]
+  match '/webhook/opens',    to: 'outbound_webhooks#open_outbound_messages',     via: [:get]
 
   resources :outbound_webhooks do
     post 'delivery', on: :collection
